f4e945752063167ba01efee8f53885bd0a037304
gringrape <53764714+gringrape@users.noreply.github.com>
2026-01-19 02:03:53 +0900

    Cleanup: Remove unused 3D decoration components (Candle, CrystalBall) and update CardDeck

--- src/components/Three/CandleModel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
-
-export function CandleModel(props: any) {
-    const { scene } = useGLTF('/models/candle_stand.glb');
-    return <primitive object={scene} {...props} />;
-}
--- src/components/Three/CrystalBallModel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
-
-export function CrystalBallModel(props: any) {
-    const { scene } = useGLTF('/models/crystal_ball.glb');
-    return <primitive object={scene} {...props} />;
-}
