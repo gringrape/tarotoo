9f3d004b49869c428f0fb4724ac165573d518730
gringrape <53764714+gringrape@users.noreply.github.com>
2026-01-19 18:42:46 +0900

    GA 설정 (react-ga4 연동)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles/GlobalStyle.ts'
 import App from './App.tsx'
+import { initGA } from './utils/analytics'
+
+initGA();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
